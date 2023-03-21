2d0299696b8a9716beb5f779d842805b34f1a792
Sharif-Miah <farhansharif141077@gmail.com>
2023-03-21 23:06:09 +0600

    chose your plan section added

--- src/Auth/firebase.config.js
+++ src/Auth/firebase.config.js
@@ -1,3 +1,23 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_apiKey,
+//     authDomain: process.env.REACT_APP_authDomain,
+//     projectId: process.env.REACT_APP_projectId,
+//     storageBucket: process.env.REACT_APP_storageBucket,
+//     messagingSenderId: process.env.REACT_APP_messagingSenderId,
+//     appId: process.env.REACT_APP_appId
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export default app;
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -5,14 +25,15 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId
+  apiKey: "AIzaSyAe5NUVSXkj-pgcwGOXP8wC00-fv2by0VY",
+  authDomain: "developed-skill.firebaseapp.com",
+  projectId: "developed-skill",
+  storageBucket: "developed-skill.appspot.com",
+  messagingSenderId: "905574040194",
+  appId: "1:905574040194:web:c685d347e2b698b10e2a9b"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export default app;
--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import PricingTable from '../PricingTable/PricingTable';
 
 const Home = () => {
 
     return (
         <div>
             <Banner />
+            <PricingTable/>
         </div>
     );
 };
